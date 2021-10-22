9cb6ee532837e5cc2d49ad6ed676a415d7f4a41f
guglielmof <ggfaggioli@gmail.com>
2021-10-22 10:13:15 +0200

    Delete python directory

--- python/qppMeasures/__init__.py
+++
@@ -1,7 +0,0 @@
-from .sARE import *
-from .sRE import *
-from .sSRE import *
-from .sSRE2 import *
-from .sRSRE import *
-from .sRSRE2 import *
-from .mappers import *
--- python/qppMeasures/mappers/RQV04.py
+++
@@ -1,5 +0,0 @@
-def RQV04(nQppModel, delimiter ="_"):
-
-    _, _, stop, stem, _, _ = nQppModel.split(delimiter)
-
-    return f"robust04_{stop}_{stem}_QL"
